110796ebd352c500fd6379d4a983e9346dd1b891
wei840222 <wei840222@gmail.com>
2021-01-29 22:21:59 +0800

    try echo bot

--- server.py
+++ server.py
@@ -1,14 +1,53 @@
+import os
+import sys
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, status
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/callback")
+def callback(response: Response):
+    # get X-Line-Signature header value
+    signature = Request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = Request.body()
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+
+    response.status_code = status.HTTP_204_NO_CONTENT
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )

3f0b42dcca5cea620b17a52913677c744adee905
wei840222 <wei840222@gmail.com>
2021-01-29 22:32:49 +0800

    fix 500 3

--- server.py
+++ server.py
@@ -38,7 +38,7 @@ async def callback(request: Request, response: Response):
 
     # handle webhook body
     try:
-        handler.handle(body, signature)
+        handler.handle(str(body), signature)
     except InvalidSignatureError:
         response.status_code = status.HTTP_400_BAD_REQUEST
 

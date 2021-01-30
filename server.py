46058411e87f08ba71657cb839c5b7358bb194c9
wei840222 <wei840222@gmail.com>
2021-01-30 09:11:28 +0800

    refactory

--- server.py
+++ server.py
@@ -1,31 +1,33 @@
 import os
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, AudioSendMessage
+import fastapi
+import linebot
+import fastapi.staticfiles as fastapiStaticfiles
+import linebot.models as linebotModels
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app = fastapi.FastAPI()
+app.mount(
+    "/static", fastapiStaticfiles.StaticFiles(directory="static"), name="static")
 
-line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None))
-bot = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET', None))
+lineBotApi = linebot.LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None))
+bot = linebot.WebhookHandler(os.getenv('LINE_CHANNEL_SECRET', None))
 base_url = os.getenv('BASE_URL', None)
 
 
 @app.post('/callback')
-async def callback(request: Request):
+async def callback(request: fastapi.Request):
     signature = request.headers['X-Line-Signature']
     body = await request.body()
     bot.handle(body.decode('utf-8'), signature)
     return 'OK'
 
 
-@bot.add(MessageEvent, message=TextMessage)
+@bot.add(linebotModels.MessageEvent, message=linebotModels.TextMessage)
 def message_text(event):
     if event.message.text == '叫':
-        line_bot_api.reply_message(
-            event.reply_token, AudioSendMessage(original_content_url=base_url + '/static/audio/noot_noot.mp3', duration=1000))
+        lineBotApi.reply_message(
+            event.reply_token, linebotModels.AudioSendMessage(
+                original_content_url=base_url + '/static/audio/noot_noot.mp3', duration=1000))
 
 
 if __name__ == '__main__':

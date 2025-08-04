0861fc8ecbd36d181b233d31579a1511cf502f58
wei840222 <wei840222@gmail.com>
2025-08-05 00:54:53 +0800

    Refactor main.py to implement lifespan context manager for FastAPI and improve error handling for environment variables

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import Request, FastAPI, HTTPException
 
 from linebot.v3.webhook import WebhookParser
@@ -22,34 +23,44 @@ config = Config()
 logger = config.get_logger()
 logger.debug("Config loaded.", config=config)  # type: ignore
 
-# Get channel_secret and channel_access_token from your environment variable
-if config.channel_secret is None:
-    raise ValueError("Specify LINE_CHANNEL_SECRET as environment variable.")
-if config.channel_access_token is None:
-    raise ValueError(
-        "Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if config.channel_secret is None:
+        raise ValueError(
+            "Specify LINE_CHANNEL_SECRET as environment variable.")
+    if config.channel_access_token is None:
+        raise ValueError(
+            "Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+
+    app.state.line_webhook_parser = WebhookParser(config.channel_secret)
+
+    configuration = Configuration(access_token=config.channel_access_token)
+    async_api_client = AsyncApiClient(configuration)
+    app.state.line_bot_api = AsyncMessagingApi(async_api_client)
+
+    yield
+
+    await async_api_client.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/callback")
 async def handle_callback(request: Request):
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers["X-LINE-Signature"]
 
     # Get request body as text
     body = await request.body()
-    body = body.decode()
-    parser = WebhookParser(config.channel_secret)
+    parser: WebhookParser = app.state.line_webhook_parser
 
     try:
-        events = parser.parse(body, signature)
+        events = parser.parse(body.decode(), signature)
     except InvalidSignatureError:
         raise HTTPException(status_code=400, detail="Invalid signature")
 
-    # Create the LINE Bot API client inside the async function
-    configuration = Configuration(access_token=config.channel_access_token)
-    async_api_client = AsyncApiClient(configuration)
-    line_bot_api = AsyncMessagingApi(async_api_client)
+    line_bot_api: AsyncMessagingApi = app.state.line_bot_api
 
     for event in events:  # type: ignore
         if not isinstance(event, MessageEvent):
